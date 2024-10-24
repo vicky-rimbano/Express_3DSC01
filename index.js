dff8b29242dc6c21d8b53a99c2b3e80e7f007e4d
vicky <vivirimbano@gmail.com>
2024-10-24 11:50:57 +0000

    link na pasta public + css menu

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3300;
+const PORT = 30012;
+
+//      configurando rota de estáticos
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  res.sand("Ola!...");
+  res.sendFile(__dirname + "/src/views/index.html");
 });
-app.listen(PORt, () => {
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
+});
+
+app.listen(PORT, () => {
   console.log("running...");
 });
